1fbbce7653261450c5a5fdb4b831ba523392bb5a
mar <marpouradressa@gmail.com>
2026-03-25 11:17:36 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,3 +1,5 @@
 //Use this file to define indexes for your queries
 db.restaurants.createIndex({ borough: 1 });
 db.restaurants.createIndex({ "location.coordinates": "2dsphere" });
+db.restaurants.createIndex({ cuisine: 1 });
+db.restaurants.createIndex({ grades.score: 1 });
