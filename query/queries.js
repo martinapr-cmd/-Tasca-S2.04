5b3f607b1bfbe052759b55e53a5e7794bcd67d1c
mar <marpouradressa@gmail.com>
2026-03-25 11:20:43 +0100

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -40,10 +40,11 @@ db.restaurants.find({"location.coordinates.0": {$lt:-95.754168}}, {_id: 0})
 
 // 11. Trobar restaurants que no preparen 'American', amb qualificació > 70 i longitud < -65.754168.
 
-db.restaurants.find({ "cuisine": {$ne: "American" } ,"grades.score":{$gt:70},"location.coordinates.0": {$lt:-65.754168}}, {_id: 0})
+db.restaurants.find({ $and: [{"cuisine": {$ne: "American"}} , {"grades.score":{$gt:70}}, {"location.coordinates.0": {$lt:-65.754168}}]}, {_id: 0})
 
 // 12. El mateix que l'anterior però sense usar operador $and.
 
+db.restaurants.find({ "cuisine": {$ne: "American" } ,"grades.score":{$gt:70},"location.coordinates.0": {$lt:-65.754168}}, {_id: 0})
 
 // 13. Trobar restaurants que no són 'American', grau 'A', i no són de Brooklyn. Ordenats per cuisine descendent.
 
